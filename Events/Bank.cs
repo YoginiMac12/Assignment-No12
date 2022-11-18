41bd5e77160b49840a301e293b0b6e37f0d3a961
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-11-18 15:47:53 +0530

    Add files via upload

---
+++ Events/Bank.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void MyDelegate();
+    public class Bank
+    {
+        public event MyDelegate Insufficientbalance; // declare an event
+        public event MyDelegate Zerobalance;
+        public event MyDelegate Lowbalance;
+        public int balance = 6000;
+
+        public int Credit(int amount)
+        {
+            balance = balance + amount;
+            return balance;
+        }
+        public void Debit(int amount)
+        {
+            if (amount > balance)
+            {
+                Insufficientbalance(); 
+            }
+            else
+            {
+                balance -= amount;
+
+                if (balance == 0)
+                {
+                    Zerobalance();
+                }
+                else if (balance < 3000)
+                {
+                    Lowbalance();
+                }
+                else
+                {
+                    //balance = balance - amount;
+                    Console.WriteLine(balance);
+                }
+
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void InsufficientMsg()
+        {
+            Console.WriteLine("You have Insufficient balance");
+        }
+        static void ZeroMsg()
+        {
+            Console.WriteLine("You have Zero balance");
+        }
+        static void LowMsg()
+        {
+            Console.WriteLine("You have Low balance");
+        }
+
+        static void Main(string[] args)
+        {
+            Bank b = new Bank();
+            b.Insufficientbalance += new MyDelegate(InsufficientMsg);
+            b.Zerobalance += new MyDelegate(ZeroMsg);
+            b.Lowbalance += new MyDelegate(LowMsg);
+            b.Credit(0);
+            b.Debit(7000);
+        }
+    }
+}
+
+
---
+++ Events/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Events
+//{
+//    public delegate void MyDelegate();
+//    public class Stud
+//    {
+//        public event MyDelegate Pass; // declare an event
+//        public event MyDelegate Fail;
+//        public void AcceptMarks(int marks)
+//        {
+//            if (marks < 40)
+//            {
+//                Fail(); // raise an event
+//            }
+//            else
+//            {
+//                Pass(); // raise an event
+//            }
+//        }
+//    }
+//    public class Program
+//    {
+//        static void PassMsg()
+//        {
+//            Console.WriteLine("You are pass");
+//        }
+//        static void FailMsg()
+//        {
+//            Console.WriteLine("You are Fail");
+//        }
+//        static void Main(string[] args)
+//        {
+//            Stud s = new Stud();
+//            s.Pass += new MyDelegate(PassMsg);
+//            s.Fail += new MyDelegate(FailMsg);
+//            s.AcceptMarks(65);
+//        }
+//    }
+//}
+
